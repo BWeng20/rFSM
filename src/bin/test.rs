12aa74d5df2aac8d7ff87f2ae05eb90f40dcb0f2
Bernd <Bernd.Wengenroth@gmx.de>
2024-05-26 01:53:02 +0200

    Started with yaml support for test specifications. Added json schema for test specifications.

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,13 +1,36 @@
+use std::ffi::OsStr;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader, Read};
+use std::path::Path;
 use std::process;
 
+#[cfg(feature = "json-config")]
 use serde::Deserialize;
+use yaml_rust::{ScanError, Yaml, YamlLoader};
+
+#[derive( Debug)]
+#[cfg_attr(feature = "json-config", derive(Deserialize))]
+pub struct EventSpecification {
+
+    /// Mandatory event name to send.
+    name: String,
+
+    /// Delay in milliseconds after the event was send.
+    delay_ms: i32,
+
+    /// Optional state to reach after the event\
+    /// Use "#stop" to check for termination of FSM.
+    shall_reach_state: Option<String>,
+
+    /// Optional event to receive from FSM after the event.
+    shall_send_event:  Option<String>
+
+}
 
 #[derive(Deserialize, Debug)]
 pub struct TestSpecification {
     file: String,
-    events: Vec<String>,
+    events: Vec<EventSpecification>,
 }
 
 #[tokio::main(flavor = "multi_thread")]
@@ -18,28 +41,73 @@ async fn main() {
     let (_trace, final_args) = rfsm::get_arguments();
 
     if final_args.len() < 1 {
-        println!("Missing argument. Please specify one or more test file");
+        println!("Missing argument. Please specify one or more test file(s)");
         process::exit(1);
     }
 
-    let file_path = final_args[0].as_str();
+    run_test_file(final_args[0].as_str());
+}
+
+pub fn run_test_file(file_path : &str) {
     match File::open(file_path) {
         Ok(file) => {
-            let reader = BufReader::new(file);
-            let v = serde_json::from_reader::<BufReader<File>, TestSpecification>(reader);
-            match v {
-                Ok(test_spec) => {
-                    run_test(test_spec);
+            let mut reader = BufReader::new(file);
+
+            let ext : String;
+            match Path::new(file_path).extension() {
+                None => {
+                    ext =  String::new()
                 }
-                Err(err) => {
-                    println!("Error deserializing test file '{}'. {}", file_path, err);
+                Some(oext) => {
+                    ext = oext.to_string_lossy().to_string();
+                }
+            }
+            match ext.to_lowercase().as_str() {
+                #[cfg(feature = "yaml-config")]
+                "yaml" | "yml" =>
+                {
+                    let mut yaml = String::new();
+                    match reader.read_to_string(&mut yaml) {
+                        Ok(_) => {
+                            match YamlLoader::load_from_str(&yaml) {
+                                Ok(doc) => {
+                                    todo!()
+                                }
+                                Err(err) => {
+                                    println!("Error parsing test file '{}'. {}", file_path, err);
+                                    process::exit(1);
+                                }
+                            }
+                        }
+                        Err(err) => {
+                            println!("Error reading test file '{}'. {}", file_path, err);
+                            process::exit(1);
+                        }
+                    }
+                }
+                #[cfg(feature = "json-config")]
+                "json" =>
+                {
+                    let v = serde_json::from_reader::<BufReader<File>, TestSpecification>(reader);
+                    match v {
+                        Ok(test_spec) => {
+                            run_test(test_spec);
+                        }
+                        Err(err) => {
+                            println!("Error deserializing test file '{}'. {}", file_path, err);
+                            process::exit(1);
+                        }
+                    }
+                }
+                _ => {
+                    println!("File '{}' has unsupported extention.", file_path );
                     process::exit(1);
                 }
             }
         }
         Err(err) => {
             println!("Error reading test file. {}", err);
-            process::exit(1);
+            process::exit(2);
         }
     }
 }
